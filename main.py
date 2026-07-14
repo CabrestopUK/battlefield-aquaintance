65a6d227e1332fb459f9ff96e6ebb9d91331e9e9
CabrestopUK <99980773+CabrestopUK@users.noreply.github.com>
2026-07-14 23:20:36 +0100

    changed frame to toplevel for menubar compatibility

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 
 window = Tk() #setup window
-frm = ttk.Frame(window, padding=10) # init frame with padding
+top = window.winfo_toplevel()
 
 #runs the event loop
 window.mainloop()
