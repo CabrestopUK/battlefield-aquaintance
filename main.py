24e3dd06b18bd758be1d2d8732c4eb74205ea2f8
CabrestopUK <99980773+CabrestopUK@users.noreply.github.com>
2026-07-17 18:22:39 +0100

    Completed menubar functionality -> added 2 new functions helpPopup and aboutPopup -> commented on code -> cleaned up code

--- main.py
+++ main.py
@@ -2,29 +2,70 @@
 from tkinter import *
 from tkinter import ttk
 
-# file window object
-class fileWindow():
-  def __init__(self, obj): # initialise window
-    self.widgets(obj) # initialise top bar widgets
+'''POPUPS'''
+def helpPopup(master):
+  """display a help popup"""
+  t = Toplevel(master) # create new window
+  
+  #set min and max size
+  t.minsize(300, 100)
+  t.maxsize(300, 100)
+  t.grid() # add grid manager
+  
+  #centre row and column 0
+  t.grid_columnconfigure(0, weight=1)
+  t.grid_rowconfigure(0, weight=1)
+  
+  #label
+  ttk.Label(t, text="Read the documentation in doc.txt in the root folder").grid(column=0, row=0)
 
-  def widgets(self, obj): #setup widgets
+def aboutPopup(master):
+  """display an about popup"""
+  t = Toplevel(master) # create new window
+  
+  #set min and max size
+  t.minsize(200, 100)
+  t.maxsize(200, 100)
+  t.grid() # add grid manager
+  
+  #centre row and column 0
+  t.grid_columnconfigure(0, weight=1)
+  t.grid_rowconfigure(0, weight=1)
+  
+  #labels
+  ttk.Label(t, text="battlefield-aquaintance", anchor="center").grid(column=0, row=0)
+  ttk.Label(t, text="A text editor", anchor="center").grid(column=0, row=1)
+  ttk.Label(t, text="Made by CabrestopUK", anchor="center").grid(column=0, row=2)
+
+class fileWindow():
+  """Make a file window"""
+  def __init__(self, master): # initialise window
+    self.widgets(master) # initialise top bar widgets
+
+  def widgets(self, master): #setup widgets
+    """initialise menubar onto master"""
     # setup menubar on object
-    menubar = Menu(obj)
-    obj.config(menu=menubar) # make menu a bar
+    menubar = Menu(master)
+    master.config(menu=menubar) # make menu a bar
     
-    '''menubar buttons/functionality'''
+    '''MENUBAR BUTTONS'''
     m1 = Menu() #first cascading menu
-    m1.add_command(label="About") # TODO add an about popup window
-    m1.add_command(label="Help") # TODO add a help popup window
+    # add buttons to cascade NOTE use command=lambda: for func with arguments in buttons
+    m1.add_command(label="About", command=lambda:aboutPopup(master)) # TODO add an about popup window
+    m1.add_command(label="Help", command=lambda:helpPopup(master)) 
     
-    menubar.add_cascade(label="Text editor", menu=m1) #add cascading menu to bar
+    menubar.add_cascade(label="battlefield-aquaintance", menu=m1) #add cascading menu to bar
 
 class app():
-  root = Tk()
+  """App class"""
+  root = Tk() # initialise tk
+
   def __init__(self):
-    fileWindow(self.root)
+    fileWindow(self.root) # create a file window
+  
   def run(self):
-    self.root.mainloop()
+    """Run the app"""
+    self.root.mainloop() # run the program event loop
 
 a = app()
 a.run()
